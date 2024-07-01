58699921c2840d7e46b46d282d7c5caf8a8d19a5
Pan Rico <juanjulianpaniagua@gmail.com>
2024-07-01 12:49:05 -0600

    Web Scraping

--- web_scraping/maplt.py
+++ web_scraping/maplt.py
@@ -1,5 +1,9 @@
-import webbrowser,sys,email
+import webbrowser,sys,email,pyperclip
 print(email.__file__)
-webbrowser.open("https://inventwithpython.com/")
+#webbrowser.open("https://inventwithpython.com/")
 if len(sys.argv)>1:
     address = ''.join(sys.argv[1:])
+else:
+    address =   pyperclip.paste()
+
+webbrowser.open('https://www.google.com/maps/place/'+address)
